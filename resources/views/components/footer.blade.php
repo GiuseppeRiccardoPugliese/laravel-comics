3cf7add007a58d450518afa16119c473995245e4
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-09 18:06:15 +0100

    Footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,17 @@
 <footer>
-    <h1>Hello from footer</h1>
+    <div class="container">
+
+        <div class="left"> <!-- BTN SIGN-UP -->
+            <button>SIGN-UP NOW!</button>
+        </div>
+
+        <div class="right"> <!-- SOCIALS W/ICONS -->
+            <span>FOLLOW US</span>
+            <a href="#"><img src="{{ Vite::asset('/public/footer-facebook.png') }}" alt=""></a>
+            <a href="#"><img src="{{Vite::asset('/public/footer-twitter.png')}}" alt=""></a>
+            <a href="#"><img src="{{Vite::asset('/public/footer-youtube.png')}}" alt=""></a>
+            <a href="#"><img src="{{Vite::asset('/public/footer-pinterest.png')}}" alt=""></a>
+            <a href="#"><img src="{{Vite::asset('/public/footer-periscope.png')}}" alt=""></a>
+        </div>
+    </div>
 </footer>
