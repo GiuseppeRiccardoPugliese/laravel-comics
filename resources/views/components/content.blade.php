01e1244c5ae189907af518a9c9921ec1ab013a79
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-09 19:39:48 +0100

    Fix components content & merch + fixing scss

--- resources/views/components/content.blade.php
+++ resources/views/components/content.blade.php
@@ -3,7 +3,7 @@
         <img class="fill" src="{{ Vite::asset('/public/jumbotron.jpg') }}" alt="">
     </div>
     <div class="container">
-        <div class="btn">
+        <div class="btn rounded-0">
             <h3 class="m-0">CURRENT SERIES</h3>
         </div>
         <div class="cards">
@@ -11,13 +11,16 @@
             <div class="thumb-card">
                 <a href="#">
                     <img src="{{$comic['thumb']}}" alt="{{$comic['type']}}">
-                    <h3>{{$comic['title']}}</h3>
+                    <h3 class="fs-5">{{$comic['title']}}</h3>
                 </a>
             </div>
             @endforeach
         </div>
-        <div class="btn load_more">
-            <a href="#">LOAD MORE</a>
+        <div class="d-flex justify-center">
+            <div class="btn load_more rounded-0">
+                <a href="#">LOAD MORE</a>
+            </div>
         </div>
+
     </div>
 </main>
--- resources/views/components/merch.blade.php
+++ resources/views/components/merch.blade.php
@@ -1,19 +1,19 @@
-<section>
+<section id="merch_sec">
+    <div class="container">
+        <ul class="container">
+            @foreach($merchs as $merch)
+                <li>
+                    <a href="#">
+                        <div class="merch_imgwrapper">
+                            <img src="{{Vite::asset($merch['src'])}}" alt="">
+                        </div>
 
-    <ul class="container">
-        @foreach($merchs as $merch)
-        <li>
-            <a href="#">
-                <div>
-                    <img src="{{Vite::asset($merch['src'])}}" alt="">
-                </div>
-
-                <span>
-                    {{ $merch['text'] }}
-                </span>
-            </a>
-        </li>
-        @endforeach
-    </ul>
-
+                        <span>
+                            {{ $merch['text'] }}
+                        </span>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </section>
