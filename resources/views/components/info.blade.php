b963d742d0735e634b079b1c4d433e13222d27a9
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-09 17:42:17 +0100

    Info section

---
+++ resources/views/components/info.blade.php
@@ -0,0 +1,112 @@
+<section class="d-flex w-100" id="section_info">
+    <div class="container d-flex justify-content-between flex-grow-1">
+
+        <div class="left"> <!-- LISTS -->
+            <div> <!-- DC COMICS & SHOP -->
+                <!-- DC COMICS -->
+                <h2 class="text-white">DC COMICS</h2>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                </ul>
+
+                <!-- SHOP -->
+                <h2 class="text-white">SHOP</h2>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div> <!-- DC -->
+                <!-- DC -->
+                <h2 class="text-white">DC</h2>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div> <!-- SITES -->
+                <!-- SITES -->
+                <h2 class="text-white">SITES</h2>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="right"> <!-- BG IMG -->
+            <img src="{{ Vite::asset('/public/dc-logo-bg.png') }}" alt="">
+        </div>
+
+    </div>
+</section>
