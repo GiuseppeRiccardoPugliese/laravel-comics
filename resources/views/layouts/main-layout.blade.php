86006b4c10466fd5de13f396e770a14f585cae3d
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-09 15:10:35 +0100

    Favicon fix w path

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('/public/favicon.ico') }}">
 
     @vite("resources/js/app.js")
 
     @yield('head')
 </head>
+
 <body>
     <div class="text-center">
         @include('components.header')
@@ -16,4 +20,5 @@
         @include('components.footer')
     </div>
 </body>
+
 </html>
